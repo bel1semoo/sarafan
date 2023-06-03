c432782aa284a12bbf44bae85d6b1f1ac8431a15
bel1semoo <mistikalapa@gmail.com>
2023-06-03 22:47:41 +0300

    123

--- js/tovar.js
+++ js/tovar.js
@@ -1,7 +1,8 @@
 function toggleAbout() {
     var aboutSection = document.querySelector('.dropdown-overlay');
     var body = document.querySelector('body');
-    if (aboutSection.style.display === 'none') {
+
+    if (aboutSection.style.display === '' || aboutSection.style.display === 'none') {
         aboutSection.style.display = 'block';
         body.style.overflow = 'hidden';
     } else {
